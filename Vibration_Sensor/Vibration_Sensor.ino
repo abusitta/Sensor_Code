ffe76df3db665c64876369562f8e86cb27a28ed9
abusitta <hbabusit@oakland.edu>
2016-02-14 19:28:40 -0500

    Fully functional vibration sensor code
    I just made it better :D

--- Vibration_Sensor/Vibration_Sensor.ino
+++ Vibration_Sensor/Vibration_Sensor.ino
@@ -1,9 +1,27 @@
+const int sensorPin = A0;
+const float baselineVibe = 0;
+
+
+
 void setup() {
-  // put your setup code here, to run once:
+Serial.begin(9600);
+
+for (int pinNumber = 2; pinNumber < 5; pinNumber++){
+  pinMode(pinNumber, OUTPUT);
+  digitalWrite(pinNumber, LOW);
+  }
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int sensorVal = analogRead(sensorPin);
 
+  Serial.print("Sensor Value: ");
+  Serial.print(sensorVal);
+
+  float Hz = (sensorVal*0.175);
+
+  Serial.print(", Hz: ");
+  Serial.println(Hz); 
+  delay(100);
 }
